8ea63f200511ba2f6ab4996f8bcce8f7e5a7607b
qiwixe <Qiwixe@gmail.com>
2026-03-05 00:58:29 +0500

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ typedef struct {
 } FileInfo;
 
 void header() {
-    printf("%-10s %-6s %-8s %-8s %-8s %-12s %s\n",
+    printf("%-10s %-6s %-6s %-6s %-10s %-12s %s\n",
                "PERM",
                "LINK",
                "OWNER",
