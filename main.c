1147f0677672aa0121405d275737ddd134e8c4f2
qiwixe <Qiwixe@gmail.com>
2026-03-04 22:59:45 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,154 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 #include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
 
-int main(int argc, char *argv[]) {   // Точка входа в программу
+typedef struct {
+    char permissions[11];
+    long links;
+    char owner[32];
+    char group[32];
+    long size;
+    char date[32];
+    char name[256];
+} FileInfo;
 
-    const char *path = (argc > 1) ? argv[1] : ".";
+void header() {
+    printf("%-11s %-6s %-10s %-10s %-10s %-16s %s\n",
+           "ПРАВА",
+           "ССЫЛКИ",
+           "ВЛАДЕЛЕЦ",
+           "ГРУППА",
+           "РАЗМЕР",
+           "ДАТА",
+           "ИМЯ ФАЙЛА");
+
+    printf("-------------------------------------------------------------------------------\n");
+}
+
+//-h
+void size() {
+
+}
+
+void generate_permissions(char *perm, mode_t mode) {
+    perm[0] = S_ISDIR(mode) ? 'd' : '-';
+    perm[1] = (mode & S_IRUSR) ? 'r' : '-';
+    perm[2] = (mode & S_IWUSR) ? 'w' : '-';
+    perm[3] = (mode & S_IXUSR) ? 'x' : '-';
+    perm[4] = (mode & S_IRGRP) ? 'r' : '-';
+    perm[5] = (mode & S_IWGRP) ? 'w' : '-';
+    perm[6] = (mode & S_IXGRP) ? 'x' : '-';
+    perm[7] = (mode & S_IROTH) ? 'r' : '-';
+    perm[8] = (mode & S_IWOTH) ? 'w' : '-';
+    perm[9] = (mode & S_IXOTH) ? 'x' : '-';
+    perm[10] = '\0';
+}
+
+//-l
+int get_info(const int lflag,const char *path, FileInfo file[] ) {
+    int count = 0;
 
     DIR *dir = opendir(path);
 
     if (!dir) {
         perror("opendir");
-        return 1;
+        return 0;
     }
 
     struct dirent *entry;
 
     while ((entry = readdir(dir)) != NULL) {
-        printf("%s\n", entry->d_name);
+        strcpy(file[count].name, entry->d_name);
+        if (lflag) {
+
+            char fullpath[1024];
+            struct stat file_stat;
+
+            snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
+            if (stat(fullpath, &file_stat) != 0) {
+                perror("stat");
+                continue;
+            }
+
+            file[count].links = file_stat.st_nlink;
+            file[count].size  = file_stat.st_size;
+
+            struct passwd *pw = getpwuid(file_stat.st_uid);
+            struct group  *gr = getgrgid(file_stat.st_gid);
+            strcpy(file[count].owner, pw ? pw->pw_name : "unknown");
+            strcpy(file[count].group, gr ? gr->gr_name : "unknown");
+
+            strftime(file[count].date, sizeof(file[count].date),
+                     "%b %d %H:%M",
+                     localtime(&file_stat.st_mtime));
+
+            generate_permissions(file[count].permissions, file_stat.st_mode);
+    }
+    count++;
+    }
+    closedir(dir);
+    return count;
+}
+
+int main(int argc, char *argv[]) {
+
+    int lflag = 0;
+    int hflag = 0;
+    int rflag = 0;
+    const char *path = ".";
+
+    FileInfo file[1024];
+    int count = 0;
+    int opt;
+
+    //Обработка аргументов
+    while ((opt = getopt(argc, argv, "lhr")) != -1) {
+        switch (opt) {
+            case 'l': lflag = 1; break;
+            case 'h': hflag = 1; break;
+            case 'r': rflag = 1; break;
+            default:
+                fprintf(stderr, "Неизвестный аргумент: -%c\n", optopt);
+                return 1;
+        }
     }
 
-    closedir(dir);
+    //Обработка пути из аргументов (Берем последний, остальные игнорируются)
+    if (optind < argc) {
+        path = argv[argc - 1];
+    }
 
+    count = get_info(lflag, path, file);
+
+    //print
+    if (lflag) {
+        header();
+        if (rflag) {
+            for (int i = 0; i < count; ++i) {
+                printf("%-11s %3ld %-8s %-8s %8ld %-12s %s\n",
+                    file[count-i].permissions,
+                    file[count-i].links,
+                    file[count-i].owner,
+                    file[count-i].group,
+                    file[count-i].size,
+                    file[count-i].date,
+                    file[count-i].name);
+            }
+        }
+            for (int i = 0; i < count; ++i) {
+                printf("%-11s %3ld %-8s %-8s %8ld %-12s %s\n",
+                    file[i].permissions,
+                    file[i].links,
+                    file[i].owner,
+                    file[i].group,
+                    file[i].size,
+                    file[i].date,
+                    file[i].name);
+        }
+    }
     return 0;
 }
